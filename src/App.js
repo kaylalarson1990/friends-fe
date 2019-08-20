5b5fafc56ddf1a6848b948e7e97a3482c0078523
Kayla <kayla.larson1990@gmail.com>
2019-08-19 18:53:55 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+  componentDidMount() {
+    return fetch("http://localhost:3001/")
+      .then(res => res.json())
+      .then(data => this.setState({ friends: data.friends }))
+      .catch(error => Error("Error fetching friends"));
+  }
+
+  render() {
+    const friendData = this.state.friends.map(friend => {
+      return <main className="friends">
+        <h3>Name: {friend.name}</h3>
+        <h3>Gender: {friend.gender}</h3>
+        <h3>Nickname: {friend.nickname}</h3>
+      </main>;
+    });
+    return <div className="App">{friendData}</div>;
+  }
+}
+
+export default App;
