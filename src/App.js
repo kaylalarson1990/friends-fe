c04e637681ca2a252961a2c2e9a9cf4471dec455
Kayla <kayla.larson1990@gmail.com>
2019-08-19 19:04:04 -0600

    Add env file

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     };
   }
   componentDidMount() {
-    return fetch("http://localhost:3001/")
+    return fetch(process.env.REACT_APP_BACKEND_URL + '/')
       .then(res => res.json())
       .then(data => this.setState({ friends: data.friends }))
       .catch(error => Error("Error fetching friends"));
